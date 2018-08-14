34308a37c0ac0ac5472297b1922e940c8f7b37bb
= <=>
2018-08-14 11:39:41 +0200

    Mongo test 3

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ MONGO_HOST="sweet-ostrich-mongodb.dev.svc.cluster.local"
 MONGO_PORT=27017
 MONGO_DBNAME="eve"
 MONGO_USERNAME="root"
-MONGO_PASSWORD=os.environ.get("MONGO_PASSWORD")
+MONGO_PASSWORD=os.environ["MONGO_PASSWORD"]
 
 DOMAIN = {'people': {}}
